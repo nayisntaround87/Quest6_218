f7c71fb90e750f31f1d45d26a4c5fba6d43598a5
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-20 14:37:23 +0700

    spacer & button

--- app/src/main/java/com/example/myarsitekturmvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/view/TampilSiswa.kt
@@ -2,7 +2,11 @@ package com.example.myarsitekturmvvm.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -58,7 +62,13 @@ fun TampilSiswa(
                     }
                     Divider(thickness = dimensionResource(1dp))
                 }
-
+                Spacer(modifier = Modifier.height(dimensionResource(10dp)))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackButtonClicked
+                ) {
+                    Text(stringResource("Back"))
+                }
             }
         }
     }

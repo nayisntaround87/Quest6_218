e4ce7c3fa01c8c8aaf77783cb307ab8250462834
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-20 14:13:45 +0700

    parameter statusUiSiswa

---
+++ app/src/main/java/com/example/myarsitekturmvvm/view/TampilSiswa.kt
@@ -0,0 +1,15 @@
+package com.example.myarsitekturmvvm.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.example.myarsitekturmvvm.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    //edit 1 : parameter statusUiSiswa
+    statusUiSiswa: Siswa,
+    onBackButtonClicked:()-> Unit
+){
+
+}

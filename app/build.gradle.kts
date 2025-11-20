51e22ee24d17b9f2d144739a15bad51a0fb4916d
nayisntaround87 <naylasalwahayati@gmail.com>
2025-11-20 09:04:02 +0700

    membuat build dan gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
+
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.navigation.compose)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
